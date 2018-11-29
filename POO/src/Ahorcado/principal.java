5fbbe618a74d8ce5c4c6806ea52df2fbe14a1e9e
diurno <diurno@pc-aula>
2018-11-29 14:01:27 +0100

    a

--- POO/src/Ahorcado/principal.java
+++ POO/src/Ahorcado/principal.java
@@ -11,37 +11,66 @@ public class principal {
         
         String comprobarLetraPalabra = null;
         
-        int intentos;
-		
+        int fallos = 0;
+
+		char charUsu;
 		int palabraAleatoria = (int) Math.round(Math.random()*palabras.length);
-			System.out.println(palabras[palabraAleatoria]);
+		System.out.println(palabras[palabraAleatoria]);
 			    
-		
+	    char coincidencias []= new char[palabras[palabraAleatoria].length()];
 			
-			for (int i = 0; i < palabras[palabraAleatoria].length(); i++) {
+		for (int i = 0; i < coincidencias.length; i++) {
 
 				System.out.print(" _ ");
 				
 				
+				
 		}
-			do {
+		System.out.println("\n");
+		do {
 			
 			comprobarLetraPalabra = JOptionPane.showInputDialog("Introduzca una letra/palabra: ");
 			
 			//Así igualamos nuestra palabra con la introducida por el usuario
-				if(comprobarLetraPalabra.equals(palabras[palabraAleatoria])) {
-					JOptionPane.showMessageDialog(null, "***Enhorabuena la palabra ha sido acertada***");
+				   if(comprobarLetraPalabra.equals(palabras[palabraAleatoria])) {
+					  JOptionPane.showMessageDialog(null, "***Enhorabuena la palabra ha sido acertada***");
 			    
-			 } 
-			    	else {
-			    		
-			    	       comprobarLetraPalabra = JOptionPane.showInputDialog("Introduzca una letra/palabra: ");
-			    	       
-			    	       if(comprobarLetraPalabra.contains(palabras[palabraAleatoria])) {
-			    	    	  // comprobarLetraPalabra.replace(_, comprobarLetraAleatoria);
+			       } else  {
+			    	   if(comprobarLetraPalabra.length()==1) {
+			    		   for(int i = 0; i < palabras[palabraAleatoria].length(); i++) {
+					           
+			    			   charUsu = comprobarLetraPalabra.charAt(0);
+			    		      if (charUsu == coincidencias[i]) {
+			    		    	  coincidencias[i] = charUsu;
+			    		    	  
+			    		    		  
+			    		    		  
+			    		    		//palabras[palabraAleatoria] = comprobarLetraPalabra;
+			    		    		 // coincidencias[i] = comprobarLetraPalabra.charAt(0);
+									//comprobarLetraPalabra.replace(coincidencias[j], charUsu);
+								
+							     System.out.print(coincidencias[i]);
+						      }
+			    		}  
+			    		   
+			    	   }
+			    	   
+			    	  /* if(palabras[palabraAleatoria].contains(comprobarLetraPalabra)) {
+			    	    	// comprobarLetraPalabra.replace(comprobarLetraPalabra, palabras[palabraAleatoria]);
+			    	    	 System.out.print(comprobarLetraPalabra);
+			    	    	 
+			    	   }*/
+			    	   else { 
+			    		   if(!palabras[palabraAleatoria].contains(comprobarLetraPalabra )) {
+			    	    	   fallos++;
+			    	    	   
+			    	    	   if(fallos >= 6){
+			    	    		   System.out.print("\n ***Has perdido***");
+			    	    	   }
 			    	       }
+			    	   }
 			             
-			 }   	
+			       }   	
 				
 			} while (!comprobarLetraPalabra.equals(palabras[palabraAleatoria]));
 			  JOptionPane.showMessageDialog(null, "***Enhorabuena la palabra ha sido acertada***");
@@ -50,6 +79,7 @@ public class principal {
 	
 		
 
+	
 
 			
 			
